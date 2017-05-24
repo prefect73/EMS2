bc4d455dbc1282b82627d0ad1c433666048e1a8b
mace120 <thedevelopers.mace@gmail.com>
2017-05-24 16:18:18 +0500

    ProjectsWrapper added.

---
+++ src/main/java/com/td/mace/wrapper/ProjectsWrapper.java
@@ -0,0 +1,22 @@
+package com.td.mace.wrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.td.mace.model.Project;
+
+public class ProjectsWrapper {
+	public List<Project> projects = new ArrayList<Project>();
+
+	public List<Project> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(
+			List<Project> projects) {
+		this.projects = projects;
+	}
+
+	
+	
+}
